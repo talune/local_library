1cf77bfad11b862013861954b22ba408f9cbcb1c
Tara Nelson <nelsont@dnc.org>
2019-03-19 14:18:45 -0700

    Install and apply standard linting and formatting packages

--- catalog/models.py
+++ catalog/models.py
@@ -1,84 +1,106 @@
+import uuid  # Required for unique book instances
+
 from django.db import models
-from django.urls import reverse # Used to generate URLS by reversing the URL patterns
-import uuid # Required for unique book instances
+from django.urls import \
+    reverse  # Used to generate URLS by reversing the URL patterns
+
 
 class Genre(models.Model):
-	"""Model representing a book genre"""
-	name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
+    """Model representing a book genre"""
 
-	def __str__(self):
-		"""String for representing the Model object."""
-		return self.name
+    name = models.CharField(
+        max_length=200, help_text="Enter a book genre (e.g. Science Fiction)"
+    )
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
+
 
 class Book(models.Model):
-	"""Model representing a book (but not a specific copy of a book)"""
-	title = models.CharField(max_length=200)
+    """Model representing a book (but not a specific copy of a book)"""
 
-	# Foreign Key used because book can only have one author,
-	# but authors can have multiple books.
-	# Author as a string rather than object because it hasn't been declared yet
-	# in the file
-	author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=200)
 
-	summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
-	isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    # Foreign Key used because book can only have one author,
+    # but authors can have multiple books.
+    # Author as a string rather than object because it hasn't been declared yet
+    # in the file
+    author = models.ForeignKey("Author", on_delete=models.SET_NULL, null=True)
 
-	# ManyToManyField used because genre can contain many books.
-	# Books can cover many genres.
-	# Genre class has already been defined so we can specify the object above.
-	genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+    summary = models.TextField(
+        max_length=1000, help_text="Enter a brief description of the book"
+    )
+    isbn = models.CharField(
+        "ISBN",
+        max_length=13,
+        help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>',
+    )
 
-	def __str__(self):
-		"""String for representing th Model object."""
-		return self.title
+    # ManyToManyField used because genre can contain many books.
+    # Books can cover many genres.
+    # Genre class has already been defined so we can specify the object above.
+    genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
 
-	def get_absolute_url(self):
-		"""Return the url to access a detail record for this book."""
-		return reverse('book-detail', args=[str(self.id)])
+    def __str__(self):
+        """String for representing th Model object."""
+        return self.title
+
+    def get_absolute_url(self):
+        """Return the url to access a detail record for this book."""
+        return reverse("book-detail", args=[str(self.id)])
+
 
 class BookInstance(models.Model):
-	"""Model representing a specific copy of a book (i.e. that can be checked out)"""
-	id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across whole library')
-	book= models.ForeignKey(Book, on_delete=models.SET_NULL, null=True)
-	imprint = models.CharField(max_length=200)
-	due_back = models.DateField(null=True, blank=True)
+    """Model representing a specific copy of a book (i.e. that can be checked out)"""
 
-	LOAN_STATUS = (
-		('m', 'Maintenance'),
-		('o', 'On Loan'),
-		('a', 'Available'),
-		('r', 'Reserved'),
-	)
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        help_text="Unique ID for this particular book across whole library",
+    )
+    book = models.ForeignKey(Book, on_delete=models.SET_NULL, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
 
-	status = models.CharField(
-		max_length=1,
-		choices=LOAN_STATUS,
-		blank=True,
-		default='m',
-		help_text='Book availability',
-	)
+    LOAN_STATUS = (
+        ("m", "Maintenance"),
+        ("o", "On Loan"),
+        ("a", "Available"),
+        ("r", "Reserved"),
+    )
 
-	class Meta:
-		ordering = ['due_back']
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default="m",
+        help_text="Book availability",
+    )
 
-	def __str__(self):
-		"""String for representing the Model object."""
-		return f'{self.id} ({self.book.title})'
+    class Meta:
+        ordering = ["due_back"]
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f"{self.id} ({self.book.title})"
+
 
 class Author(models.Model):
-  """Model representing an author."""
-  first_name = models.CharField(max_length=100)
-  last_name = models.CharField(max_length=100)
-  date_of_birth = models.DateField(null=True, blank=True)
-  date_of_death = models.DateField('Died', null=True, blank=True)
+    """Model representing an author."""
 
-  class Meta:
-    ordering = ['last_name', 'first_name']
-  
-  def get_absolute_url(self):
-    """Returns the url to access a particular author instance."""
-    return reverse('author-detail', args=[str(self.id)])
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField("Died", null=True, blank=True)
 
-  def __str__(self):
-    """String for representing the Model object."""
-    return f'{self.last_name}, {self.first_name}'
+    class Meta:
+        ordering = ["last_name", "first_name"]
+
+    def get_absolute_url(self):
+        """Returns the url to access a particular author instance."""
+        return reverse("author-detail", args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f"{self.last_name}, {self.first_name}"
--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -13,19 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-# Use include() to add paths from the catalog application
-from django.urls import include
-from django.urls import path
-# Add URL maps ot redirect the base URL to our application
-from django.views.generic import RedirectView
 # Use static() to add url mapping to serve static files during development (only)
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+# Use include() to add paths from the catalog application
+from django.urls import include, path
+# Add URL maps ot redirect the base URL to our application
+from django.views.generic import RedirectView
 
 urlpatterns = [
-  path('admin/', admin.site.urls),
-  path('catalog/', include('catalog.urls')),
-  path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+    path("admin/", admin.site.urls),
+    path("catalog/", include("catalog.urls")),
+    path("", RedirectView.as_view(url="/catalog/", permanent=True)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
