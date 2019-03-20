40ad79e1a67e337a65ca7d6c6ec5435cdc0a5ca6
Tara Nelson <nelsont@dnc.org>
2019-03-20 14:11:55 -0700

    Create a home page for catalog app
    Use context to list basic stats about books and genres.
    Use blocks to template content dynamically.

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from catalog.models import Author, Book, BookInstance, Genre
+
+def index(request):
+	"""View function for home page of site."""
+
+
+	# Generate counts of soe of the main objects
+	num_books = Book.objects.all().count()
+	num_instances = BookInstance.objects.all().count()
+
+
+	# Available books (status = 'a')
+	num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+
+	# The 'all()' is implied by default.
+	num_authors = Author.objects.count()
+	num_genres = Genre.objects.count()
+
+
+	context = {
+		'num_books': num_books,
+		'num_instances': num_instances,
+		'num_instances_available': num_instances_available,
+		'num_authors': num_authors,
+		'num_genres': num_genres,
+	}
+
+
+
+	# Render the HTML template index.html with the data in the context variable
+	return render(request, 'index.html', context=context)
+	
