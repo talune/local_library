f20237bf3c71b21301a75c168c04acbb748c5d61
Tara Nelson <nelsont@dnc.org>
2019-03-25 16:43:11 -0700

    Create book list and book detail templates

--- catalog/views.py
+++ catalog/views.py
@@ -8,20 +8,16 @@ from catalog.models import Author, Book, BookInstance, Genre
 def index(request):
 	"""View function for home page of site."""
 
-
 	# Generate counts of soe of the main objects
 	num_books = Book.objects.all().count()
 	num_instances = BookInstance.objects.all().count()
 
-
 	# Available books (status = 'a')
 	num_instances_available = BookInstance.objects.filter(status__exact='a').count()
-
 
 	# The 'all()' is implied by default.
 	num_authors = Author.objects.count()
 	num_genres = Genre.objects.count()
-
 
 	context = {
 		'num_books': num_books,
@@ -31,10 +27,14 @@ def index(request):
 		'num_genres': num_genres,
 	}
 
-
-
 	# Render the HTML template index.html with the data in the context variable
 	return render(request, 'index.html', context=context)
 
+
 class BookListView(generic.ListView):
+	model = Book
+	paginate_by = 10
+
+
+class BookDetailView(generic.DetailView):
 	model = Book
